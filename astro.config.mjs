ccef2a93687f84a1f5fdf004854162bfc31e64d4
wser <w.web.dev@gmail.com>
2023-04-26 13:58:18 +0200

    4 files modified, 2 added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 import deno from '@astrojs/deno';
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	adapter: deno(),
+  output: 'server',
+  adapter: deno(),
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
 });
